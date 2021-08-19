804fd1d81427667624f8d17fa8c190cbeca1ffe0
iTemmy <tennessee@hotmail.com.au>
2021-08-19 14:00:37 +1000

    updated PlayerArmorChangeListener to allow use of new Attribute FullSetArmorAttributes and remove attributes and status effects from players correctly.

--- src/main/java/com/temmy/json_items_test_1/listener/PlayerArmorChangeListener.java
+++ src/main/java/com/temmy/json_items_test_1/listener/PlayerArmorChangeListener.java
@@ -21,7 +21,9 @@ public class PlayerArmorChangeListener implements Listener {
     @EventHandler
     public void onPlayerArmorChange(PlayerArmorChangeEvent e){
         removeArmorEffects(e.getPlayer(), e.getOldItem());
+        removeFullSetArmorEffects(e.getPlayer(), e.getOldItem());
         removeArmorAttributes(e.getPlayer(), e.getOldItem());
+        removeFullSetAttributes(e.getPlayer(), e.getOldItem());
         ArmorEffects.checkPlayerArmor(e.getPlayer().getInventory().getArmorContents(),e.getPlayer());
         if (e.getNewItem() == null || e.getNewItem().getItemMeta() == null) return;
         Map<String, String[]> attributeMap = ItemUtils.getItemAttributeMap(e.getNewItem().getItemMeta().getPersistentDataContainer());
@@ -29,7 +31,7 @@ public class PlayerArmorChangeListener implements Listener {
             Attribute.invoke(attribute, e, attributeMap.get(attribute));
     }
 
-    private void removeArmorEffects(Player player, ItemStack armorItem){
+    private void removeArmorEffects(Player player, ItemStack armorItem) {
         if (armorItem.getItemMeta() == null) return;
         String[] effects = ItemUtils.getItemAttributeMap(armorItem.getItemMeta().getPersistentDataContainer()).get("ARMOREFFECTS");
         if (effects == null) return;
@@ -39,6 +41,10 @@ public class PlayerArmorChangeListener implements Listener {
                 if (PotionEffectType.getByName(ss) != null)
                     player.removePotionEffect(PotionEffectType.getByName(ss));
         }
+    }
+
+    private void removeFullSetArmorEffects(Player player, ItemStack armorItem) {
+        if (armorItem.getItemMeta() == null) return;
         String[] fullSetEffects = ItemUtils.getItemAttributeMap(armorItem.getItemMeta().getPersistentDataContainer()).get("FULLSETARMOREFFECTS");
         if (fullSetEffects == null) return;
         for (String s : fullSetEffects) {
@@ -49,11 +55,31 @@ public class PlayerArmorChangeListener implements Listener {
         }
     }
 
-    private void removeArmorAttributes(Player player, ItemStack armorItem){
+    private void removeArmorAttributes(Player player, ItemStack armorItem) {
         if (armorItem.getItemMeta() == null) return;
-        String[] attributes = ItemUtils.getItemAttributeMap(armorItem.getItemMeta().getPersistentDataContainer()).get("ARMORATTRIBUTEEFFECTS");
+        String[] attributes = ItemUtils.getItemAttributeMap(armorItem.getItemMeta().getPersistentDataContainer()).get("ARMORATTRIBUTES");
         if (attributes == null) return;
-        for (String s : attributes){
+        for (String s : attributes) {
+            String[] attribute = s.split(":");
+            for (int i = 0; i < attribute.length; i++) {
+                if (attribute[i].contains("health")) {
+                    attribute[i + 1] = attribute[i + 1].replace("}", "");
+                    player.getAttribute(org.bukkit.attribute.Attribute.GENERIC_MAX_HEALTH).setBaseValue(
+                            player.getAttribute(org.bukkit.attribute.Attribute.GENERIC_MAX_HEALTH).getBaseValue() - Integer.parseInt(attribute[i + 1]));
+                } else if (attribute[i].contains("speed")) {
+                    attribute[i + 1] = attribute[i + 1].replace("}", "");
+                    player.getAttribute(org.bukkit.attribute.Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(
+                            player.getAttribute(org.bukkit.attribute.Attribute.GENERIC_MOVEMENT_SPEED).getBaseValue() - Double.valueOf(attribute[i + 1]));
+                }
+            }
+        }
+    }
+
+    private void removeFullSetAttributes(Player player, ItemStack armorItem) {
+        if (armorItem.getItemMeta() == null) return;
+        String[] fullSetAttributes = ItemUtils.getItemAttributeMap(armorItem.getItemMeta().getPersistentDataContainer()).get("FULLSETARMORATTRIBUTES");
+        if (fullSetAttributes == null) return;
+        for (String s : fullSetAttributes){
             String[] attribute = s.split(":");
             for (int i = 0; i < attribute.length; i++){
                 if (attribute[i].contains("health")){
@@ -66,7 +92,6 @@ public class PlayerArmorChangeListener implements Listener {
                             player.getAttribute(org.bukkit.attribute.Attribute.GENERIC_MOVEMENT_SPEED).getBaseValue()- Double.valueOf(attribute[i+1]));
                 }
             }
-
         }
     }
 }
