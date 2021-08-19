159cf386ec8c2e5ee541269374bc69f122ce0625
iTemmy <tennessee@hotmail.com.au>
2021-08-19 14:02:51 +1000

    updated Trie name in Main class and getter.

--- src/main/java/com/temmy/json_items_test_1/attribute/CustomOre.java
+++ src/main/java/com/temmy/json_items_test_1/attribute/CustomOre.java
@@ -1,16 +1,11 @@
 package com.temmy.json_items_test_1.attribute;
 
 import com.temmy.json_items_test_1.Main;
-import com.temmy.json_items_test_1.listener.BlockBreakListener;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
-import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
@@ -31,11 +26,11 @@ public class CustomOre {
                 case COAL_ORE:
                 case DEEPSLATE_COAL_ORE:
                     if (rnd.nextInt(Main.getChances().get("Phosphorus".toLowerCase())) == 1) {
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("phosphorus"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("phosphorus"));
                         data.set(Main.getOres().get("Phosphorus"), PersistentDataType.INTEGER, 0);
                         return true;
                     }else if (data.get(Main.getOres().get("Phosphorus"), PersistentDataType.INTEGER) >= Main.getChances().get("Phosphorus".toLowerCase())-1) {
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("phosphorus"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("phosphorus"));
                         data.set(Main.getOres().get("Phosphorus"), PersistentDataType.INTEGER, 0);
                         return true;
                     }else {
@@ -48,11 +43,11 @@ public class CustomOre {
                 case DIAMOND_ORE:
                 case DEEPSLATE_DIAMOND_ORE:
                     if (rnd.nextInt(Main.getChances().get("Janelite".toLowerCase())) == 1){
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("janelite"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("janelite"));
                         data.set(Main.getOres().get("Janelite"), PersistentDataType.INTEGER,0);
                         return true;
                     }else if (data.get(Main.getOres().get("Janelite"), PersistentDataType.INTEGER) >= Main.getChances().get("Janelite".toLowerCase())-1){
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("janelite"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("janelite"));
                         data.set(Main.getOres().get("Janelite"), PersistentDataType.INTEGER,0);
                         return true;
                     }else{
@@ -62,11 +57,11 @@ public class CustomOre {
                 case EMERALD_ORE:
                 case DEEPSLATE_EMERALD_ORE:
                     if (rnd.nextInt(Main.getChances().get("Ellendyte".toLowerCase())) == 1) {
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("ellendyte"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("ellendyte"));
                         data.set(Main.getOres().get("Ellendyte"), PersistentDataType.INTEGER, 0);
                         return true;
                     }else if (data.get(Main.getOres().get("Ellendyte"), PersistentDataType.INTEGER) >= Main.getChances().get("Ellendyte".toLowerCase())-1){
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("ellendyte"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("ellendyte"));
                         data.set(Main.getOres().get("Ellendyte"), PersistentDataType.INTEGER, 0);
                         return true;
                     }else{
@@ -76,11 +71,11 @@ public class CustomOre {
                 case GOLD_ORE:
                 case DEEPSLATE_GOLD_ORE:
                     if (rnd.nextInt(Main.getChances().get("Corinthium".toLowerCase())) == 1){
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("corthium"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("corthium"));
                         data.set(Main.getOres().get("Corinthium"), PersistentDataType.INTEGER, 0);
                         return true;
                     }else if (data.get(Main.getOres().get("Corinthium"), PersistentDataType.INTEGER) >= Main.getChances().get("Corinthium".toLowerCase())-1) {
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("corinthium"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("corinthium"));
                         data.set(Main.getOres().get("Corinthium"), PersistentDataType.INTEGER, 0);
                         return true;
                     }else{
@@ -90,11 +85,11 @@ public class CustomOre {
                 case IRON_ORE:
                 case DEEPSLATE_IRON_ORE:
                     if (rnd.nextInt(Main.getChances().get("Jolixanine".toLowerCase())) == 1){
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("jolixanine"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("jolixanine"));
                         data.set(Main.getOres().get("Jolixanine"), PersistentDataType.INTEGER, 0);
                         return true;
                     }else if (data.get(Main.getOres().get("Jolixanine"), PersistentDataType.INTEGER) >= Main.getChances().get("Jolixanine".toLowerCase())-1){
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("jolixanine"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("jolixanine"));
                         data.set(Main.getOres().get("Jolixanine"), PersistentDataType.INTEGER, 0);
                         return true;
                     }else {
@@ -104,11 +99,11 @@ public class CustomOre {
                 case LAPIS_ORE:
                 case DEEPSLATE_LAPIS_ORE:
                     if (rnd.nextInt(Main.getChances().get("Sapphire".toLowerCase())) == 1){
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("Sapphire"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("Sapphire"));
                         data.set(Main.getOres().get("Sapphire"),PersistentDataType.INTEGER, 0);
                         return true;
                     }else if (data.get(Main.getOres().get("Sapphire"), PersistentDataType.INTEGER) >= Main.getChances().get("Sapphire".toLowerCase())-1){
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("sapphire"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("sapphire"));
                         data.set(Main.getOres().get("Sapphire"), PersistentDataType.INTEGER, 0);
                         return true;
                     }else{
@@ -118,11 +113,11 @@ public class CustomOre {
                 case REDSTONE_ORE:
                 case DEEPSLATE_REDSTONE_ORE:
                     if (rnd.nextInt(Main.getChances().get("Tungsten".toLowerCase())) == 1){
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("tungsten"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("tungsten"));
                         data.set(Main.getOres().get("Tungsten"), PersistentDataType.INTEGER, 0);
                         return true;
                     }else if (data.get(Main.getOres().get("Tungsten"), PersistentDataType.INTEGER) >= Main.getChances().get("Tungsten".toLowerCase())-1){
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("tungsten"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("tungsten"));
                         data.set(Main.getOres().get("Tungsten"), PersistentDataType.INTEGER, 0);
                         return true;
                     }else{
@@ -137,11 +132,11 @@ public class CustomOre {
                 case NETHER_GOLD_ORE:
                     log.info("Nether_Gold_Ore");
                     if (rnd.nextInt(Main.getChances().get("Corinthium".toLowerCase())) == 1){
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("corthium"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("corthium"));
                         data.set(Main.getOres().get("Corinthium"), PersistentDataType.INTEGER, 0);
                         return true;
                     }else if (data.get(Main.getOres().get("Corinthium"), PersistentDataType.INTEGER) >= Main.getChances().get("Corinthium".toLowerCase())-1) {
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("corinthium"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("corinthium"));
                         data.set(Main.getOres().get("Corinthium"), PersistentDataType.INTEGER, 0);
                         return true;
                     }else{
@@ -150,11 +145,11 @@ public class CustomOre {
                     }
                 case NETHER_QUARTZ_ORE:
                     if (rnd.nextInt(Main.getChances().get("Zinc".toLowerCase())) == 1){
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("zinc"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("zinc"));
                         data.set(Main.getOres().get("Zinc"), PersistentDataType.INTEGER, 0);
                         return true;
                     }else if (data.get(Main.getOres().get("Zinc"), PersistentDataType.INTEGER) >= Main.getChances().get("Zinc".toLowerCase())-1){
-                        block.getWorld().dropItem(block.getLocation(), Main.getTest().get("zinc"));
+                        block.getWorld().dropItem(block.getLocation(), Main.getCustomItems().get("zinc"));
                         data.set(Main.getOres().get("Zinc"), PersistentDataType.INTEGER, 0);
                         return true;
                     }else{
@@ -166,12 +161,6 @@ public class CustomOre {
             }
         }
         return false;
-    }
-
-    private static void test(Player player, NamespacedKey key){
-        if (player.getPersistentDataContainer().has(key, PersistentDataType.INTEGER))
-            if (Objects.equals(player.getPersistentDataContainer().get(key, PersistentDataType.INTEGER), Main.getChances().get(key.getKey().toLowerCase())))
-                log.info("player data == chance detailed in config");
     }
 
     //Iron, Gold, nether gold, Redstone, Lapis, Coal, Diamond, Quartz, Emerald, copper
