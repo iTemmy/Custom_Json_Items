375a00e191afed2b0a558d3115ddbfcb37f54ceb
iTemmy <tennessee@hotmail.com.au>
2021-09-13 15:44:22 +1000

    Updated the GiveItem command to have a fake inventoryClickEvent to activate the status effect if it is a custom item with the attribute HeldItem to provide the player with a status effect upon holding the item in the players hand.

--- src/main/java/com/temmy/json_items_test_1/command/GiveItem.java
+++ src/main/java/com/temmy/json_items_test_1/command/GiveItem.java
@@ -1,13 +1,21 @@
 package com.temmy.json_items_test_1.command;
 
 import com.temmy.json_items_test_1.Main;
+import com.temmy.json_items_test_1.listener.InventoryClickListener;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.Recipe;
 import org.jetbrains.annotations.NotNull;
 
@@ -39,5 +47,36 @@ public class GiveItem implements CommandExecutor {
         }else {
             player.sendMessage(ChatColor.YELLOW + String.format("Item '%s' doesn't exist", item));
         }
+        InventoryClickListener.onInventoryClick(new InventoryClickEvent(getView(player), InventoryType.SlotType.CONTAINER, 0, ClickType.LEFT, InventoryAction.NOTHING));
+    }
+
+    private InventoryView getView(Player player){
+        InventoryView view = new InventoryView() {
+            @Override
+            public @NotNull Inventory getTopInventory() {
+                return Bukkit.createInventory(null, 9);
+            }
+
+            @Override
+            public @NotNull Inventory getBottomInventory() {
+                return player.getInventory();
+            }
+
+            @Override
+            public @NotNull HumanEntity getPlayer() {
+                return player;
+            }
+
+            @Override
+            public @NotNull InventoryType getType() {
+                return InventoryType.PLAYER;
+            }
+
+            @Override
+            public @NotNull String getTitle() {
+                return "placeholder";
+            }
+        };
+        return view;
     }
 }
