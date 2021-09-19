28db272c5a049ec5681809698326d0aed4b0823b
iTemmy <tennessee@hotmail.com.au>
2021-09-19 18:30:35 +1000

    Fixed permission check of Reloadores command and added in section saying if person doesn't have permission to run the command.

--- src/main/java/com/temmy/json_items_test_1/command/Reload.java
+++ src/main/java/com/temmy/json_items_test_1/command/Reload.java
@@ -1,6 +1,7 @@
 package com.temmy.json_items_test_1.command;
 
 import com.temmy.json_items_test_1.Main;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,10 +10,15 @@ import org.jetbrains.annotations.NotNull;
 public class Reload implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        sender.sendMessage("Reloading ore drop chances...");
-        Main.getPlugin().reloadConfig();
-        Main.loadConfig();
-        sender.sendMessage("Reload complete!");
+        if (sender.hasPermission("jsonitems.reloadorevalues")) {
+            sender.sendMessage("Reloading ore drop chances...");
+            Main.getPlugin().reloadConfig();
+            Main.loadConfig();
+            sender.sendMessage("Reload complete!");
+            return true;
+        }
+        sender.sendMessage(ChatColor.RED+"Error: You do not have permission to run this command.");
+        sender.sendMessage(ChatColor.RED+"If you believe this to be an error please contact an admin.");
         return false;
     }
 }
