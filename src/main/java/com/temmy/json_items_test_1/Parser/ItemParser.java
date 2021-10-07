4f444dca6c59976642b188e704ba95b94151d9e7
iTemmy <tennessee@hotmail.com.au>
2021-10-08 00:30:39 +1100

    Optimized imports.

--- src/main/java/com/temmy/json_items_test_1/Parser/ItemParser.java
+++ src/main/java/com/temmy/json_items_test_1/Parser/ItemParser.java
@@ -9,7 +9,6 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.AttributeModifier;
