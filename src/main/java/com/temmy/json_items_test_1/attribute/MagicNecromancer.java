2229fc21cd8dfb30c848ba664e1edb2d08e6421c
iTemmy <tennessee@hotmail.com.au>
2022-02-12 19:17:01 +1000

    Fix for Necromancer attribute to ensure doesn't error out if player doesn't have NamespacedKey. Added permission node to bypass cooldown. Also added logger to notify console if a player tries to use necromancer with an invalid entity.
    Fix for Necromancer attribute to ensure doesn't error out if player doesn't have NamespacedKey. Added permission node to bypass cooldown. Also added logger to notify console if a player tries to use necromancer with an invalid entity.

--- src/main/java/com/temmy/json_items_test_1/attribute/MagicNecromancer.java
+++ src/main/java/com/temmy/json_items_test_1/attribute/MagicNecromancer.java
@@ -2,6 +2,7 @@ package com.temmy.json_items_test_1.attribute;
 
 import com.temmy.json_items_test_1.Main;
 import com.temmy.json_items_test_1.util.CustomDataTypes;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.NamespacedKey;
@@ -28,7 +29,7 @@ public class MagicNecromancer {
         if (!(e instanceof PlayerInteractEvent event)) return;
         if (!event.getAction().isRightClick()) return;
         Player player = event.getPlayer();
-        if (player.getPersistentDataContainer().has(necromancerCooldownKey, CustomDataTypes.Boolean)) return;
+        if (player.getPersistentDataContainer().has(necromancerCooldownKey, CustomDataTypes.Boolean)&& !player.hasPermission("jsonitems.cooldownbypass")) return;
         Location loc = player.getLocation();
         EntityType entity = null;
         int distance = 0;
@@ -38,13 +39,17 @@ public class MagicNecromancer {
             Main.getPlugin().getLogger().info(s);
             String[] ss = s.split(",");
             for (String sss : ss){
-                String[] ssc = sss.split(":");
-                if (sss.contains("entity"))
-                    entity = EntityType.valueOf(ssc[1]);
-                else if (sss.contains("distance"))
-                    distance = Integer.parseInt(ssc[1]);
-                else if (sss.contains("cooldown"))
-                    cooldown = Integer.parseInt(ssc[1])*20;
+                try {
+                    String[] ssc = sss.split(":");
+                    if (sss.contains("entity"))
+                        entity = EntityType.valueOf(ssc[1]);
+                    else if (sss.contains("distance"))
+                        distance = Integer.parseInt(ssc[1]);
+                    else if (sss.contains("cooldown"))
+                        cooldown = Integer.parseInt(ssc[1]) * 20;
+                } catch (IllegalArgumentException ex) {
+                    if (Main.debug) ex.printStackTrace();
+                }
             }
         }
         double PosX = loc.getX()+distance;
@@ -60,6 +65,13 @@ public class MagicNecromancer {
                 new Location(loc.getWorld(), loc.getX(), loc.getY(), NegZ)));
         int i = 0;
         for (Location locc : locs) {
+            if (entity == null) {
+                if (Main.debug) {
+                    Main.getPlugin().getLogger().warning(String.format("Entity on %s's %s is null", player.getName(),
+                            PlainTextComponentSerializer.plainText().serialize(event.getItem().getItemMeta().displayName())));
+                }
+                return;
+            }
             entList.add((LivingEntity) loc.getWorld().spawnEntity(locc, entity));
             Ageable age = (Ageable) entList.get(i);
             if (!age.isAdult()) age.setAdult();
@@ -79,8 +91,10 @@ public class MagicNecromancer {
             for (LivingEntity ent : entList){
                 if (ent.isDead()) continue;
                 ent.remove();
+                if (!player.getPersistentDataContainer().has(necromancerMobContainerKey, PersistentDataType.TAG_CONTAINER)) return;
                 PersistentDataContainer entContainer = player.getPersistentDataContainer().get(necromancerMobContainerKey, PersistentDataType.TAG_CONTAINER);
                 assert entContainer != null;
+                if (entContainer.getKeys() == null) return;
                 for (NamespacedKey key : entContainer.getKeys()) {
                     if (entContainer.has(key, CustomDataTypes.UUID))
                         loc.getWorld().getEntity(entContainer.get(key, CustomDataTypes.UUID));
