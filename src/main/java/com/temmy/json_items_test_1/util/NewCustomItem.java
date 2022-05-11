ba08d71566750e3a43b70e5632b0a3d9a0dc381b
iTemmy <22236166+iTemmy@users.noreply.github.com>
2022-05-11 19:31:29 +1000

    Create NewCustomItem.java

---
+++ src/main/java/com/temmy/json_items_test_1/util/NewCustomItem.java
@@ -0,0 +1,12 @@
+package com.temmy.json_items_test_1.util;
+
+public class NewCustomItem implements CustomItemInterface {
+
+  String name;
+  Material material;
+  Component displayName;
+  
+  super(name);
+  
+  
+}
